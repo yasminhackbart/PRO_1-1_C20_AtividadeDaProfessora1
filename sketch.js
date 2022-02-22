d741339a627984315af66d56101ac97d4ed0a61b
amandairizaga <97555273+amandairizaga@users.noreply.github.com>
2022-02-22 19:19:50 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+
+
+let engine;
+let world;
+var ball;
+var ground;
+var wedge;
+var angle=60;
+var poly;
+
+
+function setup() {
+  
+  
+  
+   
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+ 
+  
+}
+
