551cda43296e1c7d71e97c4943c2250efb7d4ae6
yasminhackbart <90115526+yasminhackbart@users.noreply.github.com>
2022-06-23 19:13:24 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,12 @@
 
 
-
-let engine;
-let world;
-var ball;
-var ground;
-var wedge;
-var angle=60;
-var poly;
-
-
 function setup() {
   
   
   
    
 
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
+  
 }
 
 
